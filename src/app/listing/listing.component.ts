dc5ba038672c16d39adef9f3c7b7aecb9f95af90
Levan <levanlom96@gmail.com>
2019-12-30 23:47:54 +0400

    CRUD and async await

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -7,47 +7,67 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./listing.component.scss']
 })
 export class ListingComponent implements OnInit {
-  errorMessage = '';
-  searcKeyword = '';
-  movies = [];
+  genres: any = [];
+  genreName = '';
+  editGenreName = '';
+  selectedGenre = null
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() { }
 
-  makeSearch() {
-    let params = new HttpParams();
-    params = params.append('page', '1');
-    params = params.append('r', 'json');
-    params = params.append('s', this.searcKeyword);
+  // Async -> await
+  async getAllGenres() {
+    try {
+      const promise = await this.httpClient.get('http://localhost:3200/api/genres').toPromise();
+      this.genres = promise;
 
-    let headers = new HttpHeaders();
-    headers = headers.append('x-rapidapi-host', 'movie-database-imdb-alternative.p.rapidapi.com');
-    headers = headers.append('x-rapidapi-key', '5f762cbb66mshcfebf6533c33f45p105831jsn18c98477fe1f');
+    } catch (error) {
+      console.log('Error');
+    }
+  }
 
-    const url = 'https://movie-database-imdb-alternative.p.rapidapi.com/';
-    const options = { headers: headers, params: params };
+  selectGenre(item) {
+    this.selectedGenre = item;
+  }
 
-    this.httpClient.get(url, options).toPromise().then(
-      (data: any) => { this.movies = data.Search; }
-    ).catch(
-      (error) => {
-        console.log('Error: ', error);
-        this.errorMessage = error.error.message;
-      }
-    )
+  addNewGenre() {
+    const request = {
+      name: this.genreName
+    };
 
-    // *** Es aris observable
-    // this.httpClient.get(url, options).subscribe(
-    //   (data: any) => {
-    //     console.log('Data: ', data);
-    //     this.movies = data.Search;
-    //   },
-    //   (error) => {
-    //     console.log('Error: ', error);
-    //     this.errorMessage = error.error.message;
-    //   }
-    // );
+    this.httpClient.post('http://localhost:3200/api/genres', request).toPromise().then(
+      () => { this.getAllGenres() },
+      (err) => { console.log(err) }
+    );
+  }
+
+  editGenreNameAction() {
+    if (!this.selectedGenre) { return }
+
+    const request = {
+      name: this.editGenreName
+    };
+
+    this.httpClient.put(`http://localhost:3200/api/genres/${this.selectedGenre.id}`, request).toPromise().then(
+      (data: any) => {
+        this.getAllGenres();
+        this.selectedGenre.name = data.name;
+      },
+      (err) => { console.log(err) }
+    );
+  }
+
+  deleteGenre() {
+    if (!this.selectedGenre) { return }
+
+    this.httpClient.delete(`http://localhost:3200/api/genres/${this.selectedGenre.id}`).toPromise().then(
+      () => {
+        this.getAllGenres();
+        this.selectedGenre = null;
+      },
+      (err) => { console.log(err) }
+    );
   }
 
 }
